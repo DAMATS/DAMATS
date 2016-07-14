29e10100b0121949469af45db240b8687d3a10b0
Martin Paces <martin.paces@eox.at>
2016-07-14 11:26:34 +0300

    Preserving positional arguments with spaces in the manage.py wrapper.

--- scripts/manage.sh
+++ scripts/manage.sh
@@ -6,4 +6,4 @@
 [ -f "`dirname $0`/user.conf" ] && . `dirname $0`/user.conf
 . `dirname $0`/lib_common.sh
 
-sudo -u "$DAMATS_USER" python "$DAMATS_SERVER_HOME/manage.py" $*
+sudo -u "$DAMATS_USER" python "$DAMATS_SERVER_HOME/manage.py" "$@"
