3a0aca25c3857d1abeb398a693af0ded9f3b0526
Martin Paces <martin.paces@eox.at>
2016-07-23 10:58:42 +0200

    Minor improvement of the restart script.

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/sh -x
 sudo systemctl restart eoxs_wps_async.service
+sleep 1
 sudo systemctl restart httpd.service
+sleep 1
 sudo systemctl status httpd.service
-sleep 5
+sleep 10
 sudo systemctl status eoxs_wps_async.service
